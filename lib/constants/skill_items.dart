c6726b7abe0310bb561b463d4d6e8d2b25562c27
Orhan Demir <orhandem4@gmail.com>
2025-08-04 17:56:23 +0300

    three

---
+++ lib/constants/skill_items.dart
@@ -0,0 +1,45 @@
+const List<Map> platformItems = [
+  {
+    "img" : "assets/android_icon.png",
+    "title" : "Android Dev",
+  },
+  {
+    "img" : "assets/web_icon.png",
+    "title" : "Web Dev",
+  },
+  {
+    "img" : "assets/ios_icon.png",
+    "title" : "IOS Dev",
+  },
+  {
+    "img" : "assets/desktop_icon.png",
+    "title" : "Desktop Dev",
+  },
+];
+
+const List<Map> skillItems = [
+  {
+    "img" : "assets/bootstrap_icon.png",
+    "title" : "Bootstrap",
+  },
+  {
+    "img" : "assets/css_icon.png",
+    "title" : "CSS3",
+  },
+  {
+    "img" : "assets/dart_icon.png",
+    "title" : "Dart",
+  },
+  {
+    "img" : "assets/flutter_icon.png",
+    "title" : "Flutter",
+  },
+  {
+    "img" : "assets/html_icon.png",
+    "title" : "HTML5",
+  },
+  {
+    "img" : "assets/ajavascript_icon.png",
+    "title" : "JS",
+  },
+];
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:portfolio_easy/constants/colors.dart';
 import 'package:portfolio_easy/constants/size.dart';
+import 'package:portfolio_easy/constants/skill_items.dart';
 import 'package:portfolio_easy/widgets/drawer_mobile.dart';
 import 'package:portfolio_easy/widgets/header_desktop.dart';
 import 'package:portfolio_easy/widgets/header_mobile.dart';
@@ -42,16 +43,53 @@ class _HomePageState extends State<HomePage> {
                     scaffoldKey.currentState?.openEndDrawer();
                   },
                 ),
-              if (Constraints.maxWidth >= kMinDesktopWidth) 
-              const MainDesktop()
-              else 
-              MainMobile(),
+              if (Constraints.maxWidth >= kMinDesktopWidth)
+                const MainDesktop()
+              else
+                MainMobile(),
 
               // SKILLS
               Container(
-                height: 500,
-                width: double.maxFinite,
-                color: Colors.blueGrey,
+                width: screenWidth,
+                padding: const EdgeInsets.fromLTRB(25, 20, 25, 60),
+                color: CustomColor.bgLight1,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    //title
+                    Text(
+                      "Neler Yapabilirim",
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: CustomColor.whitePrimary,
+                      ),
+                    ),
+                    // platforms and skills
+                    Row(
+                      children: [
+                        //platform
+                        Wrap(
+                          children: [
+                            for(int i=0; i<platformItems.length; i++)
+                            Container(
+                              width: 200,
+                              decoration: BoxDecoration(
+                                color: CustomColor.bgLight2,
+                                borderRadius: BorderRadius.circular(5),
+                              ),
+                              child: ListTile(
+                                leading: Image.asset(""),
+                                title: Text(""),
+                              ),
+                            )
+                          ],
+                        ),
+                        //skills
+                      ],
+                    ),
+                  ],
+                ),
               ),
               // PROJECTS
               Container(height: 500, width: double.maxFinite),
